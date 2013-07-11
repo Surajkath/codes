a112e04ffe2b9e38dbbbe3b00d09993aef5e0b82
suraj <surajkath108@gmail.com>
2013-07-11 21:46:35 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,163 +1,118 @@
 
-//Shortest sub Segment
+//Connected Sets
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+ Enter your code here. Read input from STDIN. Print output to STDOUT
+ Your class should be named Solution
+*/
 
 import java.io.*;
-/**
- *
- * @author SURAJKA
- */
+ class ConnectedSets {
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-import java.util.HashMap;
-
-/**
- *
- * @author SURAJKA
- */
- class ShortestSegment {
+    /**
+     * @param args the command line arguments
+     */
     
-
-    int beginStr;
-    int endStr;
-    int length;
-    int minimumWindowLen=200000000;
-    HashMap<String,Integer>needtofind=new HashMap<String, Integer>();
-    HashMap<String,Integer>hasfound=new HashMap<String, Integer>();
+    int n;
+    int a[][];
+    int visited[][];
     
-    void decrementHasFoundmap(String str)
+    
+    ConnectedSets(int a[][],int n)
     {
-        
-         
-        if(hasfound.containsKey(str))
-        {
-            
-            int i=hasfound.get(str);
-            hasfound.put(str, i-1);
-            
-        }
+        this.a=a;
+        this.n=n;
+        visited=new int[n][n];
     }
-    void incermentHasFound(String str)
-    {
-        
-         
-        if(hasfound.containsKey(str))
-        {
-            
-            int i=hasfound.get(str);
-            hasfound.put(str, i+1);
-            
-        }
-    else
-        hasfound.put(str,1);
-    }
-    void initialiseMaps(String str)
-    {
-        
-        if(needtofind.containsKey(str))
-        {
-            
-            int i=needtofind.get(str);
-            needtofind.put(str, i+1);
-            
-        }
-        else
-        needtofind.put(str,1);
-    }
+           
+    int connectedComponenet;
     
-    
-    void findminimumWindow(String str)
+    public void findConnectedComponenet()
     {
-        
-       
-        String strArray[]=str.split(" ");
-        int count=0;
-        
-        
-        int begin=0,end=0;
-        for(begin=0,end=0;end<strArray.length;end++)
+        for(int i=0;i<n;i++)
         {
-            
-            if(!needtofind.containsKey(strArray[end]))
-                continue;
-            incermentHasFound(strArray[end]);
-            if(hasfound.get(strArray[end])<=needtofind.get(strArray[end]))
-                count++;
-            
-            if(count==length)
+            for(int j=0;j<n;j++)
             {
-                while(needtofind.get(strArray[begin])==null||hasfound.get(strArray[begin])>needtofind.get(strArray[begin]))
+                if(a[i][j]==1&&visited[i][j]!=1)
                 {
-                   
-                    if(needtofind.get(strArray[begin])!=null&&hasfound.get(strArray[begin])>needtofind.get(strArray[begin]))
-                      decrementHasFoundmap(strArray[begin]);
-                       begin++;
+                    connectedComponenet++;
+                    findComponenet(i, j);
                 }
-                    
-             
-                int windowlength=end-begin+1;
-                if(windowlength<minimumWindowLen)
-                {
-                    minimumWindowLen=windowlength;
-                    beginStr=begin;
-                    endStr=end;
-                }
-            
             }
-            
         }
-        
+    
+        System.out.println(connectedComponenet);
     }
+
+    private void findComponenet(int i,int j)
+    {
+        
+                
+            if(i<0 ||i>=n||j<0||j>=n)
+                {
+                    return;
+                }
+        
+            if(visited[i][j]==1||a[i][j]==0)
+                    return ;
+                
+                
+                
+                
+                
+                visited[i][j]=1;
+                
+                findComponenet(i-1, j-1);
+                findComponenet(i-1, j);
+                findComponenet(i-1, j+1);
+                findComponenet(i, j-1);
+                findComponenet(i, j+1);
+                
+                findComponenet(i+1, j-1);
+                
+                
+                findComponenet(i+1, j);
+                
+                
+                findComponenet(i+1, j+1);
+                
+                
+                
+    
+    }
+    
+
+
 
 }
 
+
+
 public class Solution {
-    
-    
-    public static void main(String args[])throws Exception
-    {
-        ShortestSegment obj=new ShortestSegment();
-        File fil=new File("abc");
+    public static void main(String args[] ) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
         String line = br.readLine();
-        int noofwords=Integer.parseInt(br.readLine());
-        obj.length=noofwords;
-        for(int i=0;i<noofwords;i++)
+        int N = Integer.parseInt(line);
+        for(int i=0;i<N;i++)
         {
-            obj.initialiseMaps(br.readLine().toUpperCase());
+            line = br.readLine(); 
+            int dimension=Integer.parseInt(line);
+            int a[][]=new int[dimension][dimension];
+            
+            for(int j=0;j<dimension;j++)
+            {
+                 line = br.readLine(); 
+                 String element[]=line.split(" ");
+            
+                 for(int k=0;k<element.length;k++)
+                 {
+                     a[j][k]=Integer.parseInt(element[k]);
+                 }
+            }
+            
+            ConnectedSets obj=new ConnectedSets(a, dimension);
+            obj.findConnectedComponenet();
             
         }
-        
-        
-        line=line.replaceAll("[^a-zA-Z]", " ");
-        line=line.replaceAll("  ", " ");
-        while(line.indexOf("  ")>-1)
-        {
-          line=line.replaceAll("  ", " ");  
-        }
-        obj.findminimumWindow(line.toUpperCase());
-        String strArray[]=line.split(" ");
-        
-        
-        if(obj.endStr==0)
-        {
-            System.out.println("NO SUBSEGMENT FOUND");
-        }
-        else
-        for(int i=obj.beginStr;i<=obj.endStr;i++)
-        {
-            System.out.print(strArray[i]+" ");
-        }
     }
-    
 }
